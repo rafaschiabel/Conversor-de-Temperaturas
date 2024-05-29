f0b95c4124ba4fdb619797cc0b207d8af48f710e
Rafael Schiabel <rafael.schiabel@gmail.com>
2024-05-28 21:42:37 -0300

    v1

--- conversor.py
+++ conversor.py
@@ -11,10 +11,31 @@ print('(K) Kelvin')
 print('')
 print('>> Digite as letras correspondentes')
 
-ini = input('DE: ')
-fim = input('PARA: ')
-valor = float(input('VALOR: '))
+# Conversão de string para float, substituindo , por .
+def converter(num_string):
+    if ',' in num_string:
+        return num_string.replace(',','.')
+    return float(num_string)
 
+# Verificador de números
+def verifica_num(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+ini = input('DE: ').upper()
+fim = input('PARA: ').upper()
+valor = input('VALOR: ')
+
+while verifica_num(valor) == False:
+    print('Valor inválido!')
+    valor = input('VALOR: ')
+valor = converter(valor)
+
+
+# CONVERSOR
 if ini in ['C','F','K'] and fim in ['C','F','K']:
     # CELSIUS / KELVIN
     if ini == 'C' and fim == 'K':
@@ -24,14 +45,15 @@ if ini in ['C','F','K'] and fim in ['C','F','K']:
 
     # CELSIUS / FAHRENHEIT
     if ini == 'C' and fim == 'F':
-        retorno = (1,8 * valor) + 32
+        retorno = (1.8 * valor) + 32
     if ini == 'F' and fim == 'C':
-        retorno = (valor - 32) / 1,8
+        retorno = (valor - 32) / 1.8
 
     # KELVIN / FAHRENHEIT
+    if ini == 'F' and fim == 'K':
+        retorno = ((valor - 32) * 5/9) + 273.15
     if ini == 'K' and fim == 'F':
-        
-        retorno = (1,8 * valor) + 32
+        retorno = ((valor - 273.15) * 9/5) + 32
         
     print(retorno)
 else:
